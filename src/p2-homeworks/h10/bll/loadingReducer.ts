f301ea36c8dfb5a57e4c977d929cf9acfbdfbcd5
Denis Tavabilov <dagger1233@gmail.com>
2022-05-14 01:04:11 +0500

    end 10dzignat

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,34 @@
-const initState = {
+const CHANGE_LOADING = 'CHANGE_LOADING'
 
+export type StateType = {
+    loading:boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const initState = {
+    loading: false
+}
+
+export const loadingReducer = (state:StateType = initState, action: loadingACType): StateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case "CHANGE_LOADING":{
+            return {
+                ...state,
+                loading:action.value
+            }
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type loadingACType = {
+    type:typeof CHANGE_LOADING
+    value:boolean
+}
+
+export const loadingAC = (value:boolean): loadingACType => {
+    return {
+        type:CHANGE_LOADING,
+        value
+    }
+} // fix any
