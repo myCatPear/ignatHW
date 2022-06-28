2c6385ca172eb4237d67fc66d90ad097ead1f88c
Denis Tavabilov <dagger1233@gmail.com>
2022-06-28 16:39:04 +0500

    feat HW 11, add doublerange

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -7,6 +7,11 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const changeRangeHandler = (value: number[]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
     return (
         <div>
             <hr/>
@@ -22,10 +27,12 @@ function HW11() {
                 />
             </div>
 
-            <div>
-                <span>{value1}</span>
+            <div className={s.superDoubleRange}>
+                <span className={s.value1}>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    value={[value1,value2]}
+                    onChangeRange={changeRangeHandler}
                 />
                 <span>{value2}</span>
             </div>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,52 @@
+import Slider from '@mui/material/Slider/Slider'
 import React from 'react'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange: (value: number[]) => void
+    value: [number, number]
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        onChangeRange,
+        value,
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
+    // const handleChange = (event: Event, newValue: number | number[]) => {
+    //     if (onChangeRange) {
+    //         onChangeRange(newValue as number[]);
+    //     }
+    // };
+
+    const minDistance = 1;
+
+    const handleChange1 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            onChangeRange([Math.min(newValue[0], value[1] - minDistance), value[1]]);
+        } else {
+            onChangeRange([value[0], Math.max(newValue[1], value[0] + minDistance)]);
+        }
+    };
 
     return (
         <>
-            DoubleRange
+            <Slider
+                getAriaLabel={() => 'Temperature range'}
+                value={value}
+                onChange={handleChange1}
+                valueLabelDisplay="auto"
+            />
         </>
     )
 }
